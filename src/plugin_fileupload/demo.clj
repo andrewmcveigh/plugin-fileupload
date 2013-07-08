a42ea960eef11635d003e2884c42abb7fabec3d5
Andrew Mcveigh <me@andrewmcveigh.com>
2013-07-08 20:48:15 +0200

    Remove loading js from resources

--- src/plugin_fileupload/demo.clj
+++ src/plugin_fileupload/demo.clj
@@ -11,7 +11,7 @@
     [ring.middleware.resource :as resource]
     [ring.middleware.file-info :as file-info]
     [ring.middleware.keyword-params :as kparams]
-    [hiccup.core :refer [html]])
+    [hiccup.page :refer [html5]])
   (:import
     [java.io FileOutputStream InputStream OutputStream]))
 
@@ -65,42 +65,63 @@
    ;display: inline;
    ;}"])
 
+(def load-js
+  [:script
+  "
+/*jslint unparam: true */
+/*global window, $ */
+$(function () {
+'use strict';
+// Change this to the location of your server-side upload handler:
+var url = window.location.hostname === 'blueimp.github.io' ?
+'//jquery-file-upload.appspot.com/' : 'server/php/';
+$('#fileupload').fileupload({
+url: url,
+dataType: 'json',
+done: function (e, data) {
+$.each(data.result.files, function (index, file) {
+$('<p/>').text(file.name).appendTo('#files');
+});
+},
+progressall: function (e, data) {
+var progress = parseInt(data.loaded / data.total * 100, 10);
+$('#progress .bar').css(
+'width',
+progress + '%'
+);
+}
+})
+});
+  "])
 
 
 (def test-page
-  (html [:html
-         [:head
-          [:link
-           {:rel "stylesheet"
-            :href "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css"}]
-          [:link
-           {:rel "stylesheet"
-            :href "http://localhost:3000/plugins/fileupload/css/style.css"}]
-          fileupload/css
-          ]
-         [:body
-          [:div.container
-            [:div.row
-             [:div.span12
-              [:h1 "test"]]]
-           [:div.row
-            [:span.btn.btn-success.fileinput-button
-             [:i.icon-plus.icon-white] \space
-             [:span "Select Files..."]
-             [:input#fileupload {:type :file :name "files[]" :multiple true :data-url "/upload"}]]]
-           [:div.row
-            [:div#progress.progress.progress-success.progress-striped
-             [:div.bar]
-             ]
-            ]
-           [:div.row [:div#files.files]]
-           ]
-          (fileupload/js :jquery true
-                         :jquery-ui true
-                         :process true
-                         :validate true)
-          [:script {:src "/js/main.js"}]
-          ]]))
+  (html5
+    [:head
+     [:link
+      {:rel "stylesheet"
+       :href "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css"}]
+     [:link {:rel "stylesheet" :href "/plugins/fileupload/css/style.css"}]
+     fileupload/css ]
+    [:body
+     [:div.container
+      [:div.row
+       [:div.span12
+        [:h1 "test"]]]
+      [:div.row
+       [:span.btn.btn-success.fileinput-button
+        [:i.icon-plus.icon-white] \space
+        [:span "Select Files..."]
+        [:input#fileupload {:type :file :name "files[]" :multiple true :data-url "/upload"}]]]
+      [:div.row
+       [:div#progress.progress.progress-success.progress-striped
+        [:div.bar]]]
+      [:div.row [:div#files.files]]]
+     (fileupload/js :jquery true
+                    :jquery-ui true
+                    :process true
+                    :validate true)
+     load-js]))
 
 (defroutes app-routes
   (GET "/upload" request
@@ -110,9 +131,7 @@
   (POST "/upload" request (do (pprint request) (prn) (handler "/tmp" request)
                               (response-success-merge
                                 {:files (mapv #(hash-map :name (:filename %))
-                                              (:files (:params request)))}
-                                )
-                              ))
+                                              (:files (:params request)))})))
   (GET "/" [] test-page)
   (route/not-found "Not Found"))
 

27b25587a71423f949aa4af0bb0f6c1bf21f04eb
Andrew Mcveigh <me@andrewmcveigh.com>
2013-07-08 20:58:55 +0200

    Moved handler fns to core

--- src/plugin_fileupload/core.clj
+++ src/plugin_fileupload/core.clj
@@ -1,6 +1,12 @@
 (ns plugin-fileupload.core
   (:require
-    [plugin-jquery.core :as jquery]))
+    [plugin-jquery.core :as jquery]
+    [clojure.string :as string]
+    [clojure.data.json :as json]
+    [clojure.java.io :as io]
+    [ring.middleware.keyword-params :as kparams])
+  (:import
+    [java.io FileOutputStream InputStream OutputStream]))
 
 (def css
   [:link {:rel "stylesheet"
@@ -24,3 +30,30 @@
        (when process (fileupload "process"))
        (when validate (fileupload "validate"))
        (when video (fileupload "video"))])))
+
+(defn ->map [path {:keys [params multipart-params] :as request}]
+  (let [multipart-params (#'kparams/keyify-params multipart-params)
+        multipart? (> (count multipart-params) 0)
+        _ (prn params)
+        {{filename :filename} "files[]"
+         :as params} (if multipart? multipart-params params)
+        filename (last (string/split filename #"\\"))]
+    {:body (if multipart? (:tempfile (params "files[]")) (:body request))
+     :chunk-index (Integer. (params "chunk" 0))
+     :params (assoc-in params [:files :filename] filename)
+     :path (str path "/" filename)}))
+
+(defn handle-file [{:keys [body path chunk-index]}]
+  (io/copy (try (cast InputStream body) (catch ClassCastException _ body))
+           (cast OutputStream
+                 (FileOutputStream. path (not= 0 chunk-index)))))
+
+(defn handler
+  "Saves uploaded file to \"path\" location, from request."
+  [path request]
+  (handle-file (->map path request)))
+
+(defn response-success-merge [m]
+  {:status 200
+   :headers {"Content-Type" "application/json"}
+   :body (json/write-str (merge {:success true} m))})
--- src/plugin_fileupload/demo.clj
+++ src/plugin_fileupload/demo.clj
@@ -1,69 +1,14 @@
 (ns plugin-fileupload.demo
   (:require
-    [plugin-fileupload.core :as fileupload]
-    [clojure.string :as string]
-    [clojure.java.io :as io]
-    [clojure.data.json :as json]
+    [plugin-fileupload.core :as fileupload
+     :refer [handler response-success-merge]]
     [clojure.pprint :refer [pprint]]
     [compojure.core :refer [defroutes GET POST]]
     [compojure.handler :as handler]
     [compojure.route :as route]
     [ring.middleware.resource :as resource]
     [ring.middleware.file-info :as file-info]
-    [ring.middleware.keyword-params :as kparams]
-    [hiccup.page :refer [html5]])
-  (:import
-    [java.io FileOutputStream InputStream OutputStream]))
-
-(defn ->map [path {:keys [params multipart-params] :as request}]
-  (let [multipart-params (#'kparams/keyify-params multipart-params)
-        multipart? (> (count multipart-params) 0)
-        _ (prn params)
-        {{filename :filename} "files[]"
-         :as params} (if multipart? multipart-params params)
-        filename (last (string/split filename #"\\"))]
-    {:body (if multipart? (:tempfile (params "files[]")) (:body request))
-     :chunk-index (Integer. (params "chunk" 0))
-     :params (assoc-in params [:qqfile :filename] filename)
-     :path (str path "/" filename)}))
-
-(defn handle-file [{:keys [body path chunk-index]}]
-  (io/copy (try (cast InputStream body) (catch ClassCastException _ body))
-           (cast OutputStream
-                 (FileOutputStream. path (not= 0 chunk-index)))))
-
-(defn handler
-  "Saves uploaded file to \"path\" location, from request."
-  [path request]
-  (handle-file (->map path request)))
-
-(def response-success
-  {:status 200
-   :headers {"Content-Type" "application/json"}
-   :body "{\"success\": true}"})
-
-(defn response-success-merge [m]
-  {:status 200
-   :headers {"Content-Type" "application/json"}
-   :body (json/write-str (merge {:success true} m))})
-
-;(def bootstrap-style-block
-  ;[:style {:type "text/css"}
-   ;" /* Fine Uploader
-   ;-------------------------------------------------- */
-   ;.qq-upload-list {
-   ;text-align: left;
-   ;}
-   ;/* For the bootstrapped demos */
-   ;li.alert-success {
-   ;background-color: #DFF0D8;
-   ;}
-   ;li.alert-error {
-   ;background-color: #F2DEDE;
-   ;}
-   ;.alert-error .qq-upload-failed-text {
-   ;display: inline;
-   ;}"])
+    [hiccup.page :refer [html5]]))
 
 (def load-js
   [:script
@@ -102,7 +47,7 @@ progress + '%'
       {:rel "stylesheet"
        :href "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css"}]
      [:link {:rel "stylesheet" :href "/plugins/fileupload/css/style.css"}]
-     fileupload/css ]
+     fileupload/css]
     [:body
      [:div.container
       [:div.row
